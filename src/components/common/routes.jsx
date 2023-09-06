2d1ddb3f052292a770d246ba732eadb776f7ce82
Rahul Srigadde <rahul.srigadde625@gmail.com>
2023-09-06 16:20:46 +0530

    react-router-dom setup

--- src/components/common/routes.jsx
+++ src/components/common/routes.jsx
@@ -1,12 +1,33 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { MENU_ITEMS } from "../../constants/constant";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
-function Routes(props) {
-    return (
-        <Typography component="h2" variant="h6" color="primary" gutterBottom>
-            {props.children}
-        </Typography>
-    );
+function ApplicationRoutes(props) {
+  return (
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Routes>
+          {MENU_ITEMS.map((ele) => (
+            <Route path={ele.path} element={ele.component} />
+          ))}
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Container>
+    </Box>
+  );
 }
 
-export default Routes;
+export default ApplicationRoutes;
--- src/components/listItems.jsx
+++ src/components/listItems.jsx
@@ -1,33 +1,23 @@
-import * as React from 'react';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { MENU_ITEMS } from "../constants/constant";
+import { useNavigate } from "react-router-dom";
 
-const items = [
-    {name : 'Dashboard', icon: <DashboardIcon />, path: '/dashboard'},
-    {name : 'Reports', icon: <DashboardIcon />, path: '/reports'},
-    {name : 'Get Started', icon: <DashboardIcon />, path: '/get_started'}
-
-];
-
-export const mainListItems = (
+function MainListItems() {
+  const navigate = useNavigate();
+  return (
     <React.Fragment>
-        {items.map(element => (
-            <ListItemButton onClick={() => alert(element.path)}>
-                <ListItemIcon>
-                    {element.icon}
-                </ListItemIcon>
-                <ListItemText primary={element.name} />
-            </ListItemButton>
-        ))};
+      {MENU_ITEMS.map((element) => (
+        <ListItemButton onClick={() => navigate(element.path)}>
+          <ListItemIcon>{element.icon}</ListItemIcon>
+          <ListItemText primary={element.name} />
+        </ListItemButton>
+      ))}
+      ;
     </React.Fragment>
-);
+  );
+}
 
-
+export default MainListItems;
